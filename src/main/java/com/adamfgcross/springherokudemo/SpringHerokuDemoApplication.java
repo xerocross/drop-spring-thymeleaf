dd7fc38af271584f2314d04434c3e68b1c172f26
Adam Cross <adamfgcross@gmail.com>
2021-09-22 10:02:06 -0400

    initial commit

---
+++ src/main/java/com/adamfgcross/springherokudemo/SpringHerokuDemoApplication.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.springherokudemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHerokuDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHerokuDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamfgcross/springherokudemo/SpringHerokuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.springherokudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHerokuDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
