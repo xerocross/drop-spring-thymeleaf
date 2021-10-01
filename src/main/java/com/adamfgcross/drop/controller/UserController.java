ea690ca0b91949955737c57c2e2ffdcac6007838
Adam Cross <adamfgcross@gmail.com>
2021-10-01 08:00:04 -0400

    cleanup

--- src/main/java/com/adamfgcross/drop/controller/UserController.java
+++ src/main/java/com/adamfgcross/drop/controller/UserController.java
@@ -5,9 +5,7 @@ import com.adamfgcross.drop.entity.User;
 import com.adamfgcross.drop.exception.UserAlreadyExistsException;
 import com.adamfgcross.drop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,16 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
     public String viewLoginPage(User user) {
@@ -57,4 +51,3 @@ public class UserController {
         }
     }
 }
-
